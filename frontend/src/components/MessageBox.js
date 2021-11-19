a3944c05f1e311e2217b61e5acd3a7404f62591c
rutujal <rutujalabhshetwar2002@gmail.com>
2021-11-19 21:04:29 +0530

    Handled add cart button

--- frontend/src/components/MessageBox.js
+++ frontend/src/components/MessageBox.js
@@ -4,8 +4,8 @@ export default function MessageBox(props) {
   console.log(props);
   return (
     <div className={`alert alert-${props.variant || 'info'}`}>
-      {/* {props.children} */}
-      <p>Something went wrong</p>
+      {props.children} 
+     
     </div>
   );
 }
