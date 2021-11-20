42216c5e6e722d8ccac9df3c12f143284e70d19b
vaishnavi6122 <vaishnavid6122@gmail.com>
2021-11-20 11:21:12 +0530

    authorisation is done

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,15 @@
 // "experss": "0.0.1-security",
 import express from 'express';
 import  mongoose  from 'mongoose';
-import data from './data.js';
+import dotenv from 'dotenv';
+dotenv.config({path:'../env'});
 import userRouter from './routers/userRouter.js';
-
+import productRouter from './routers/productRouter.js';
 
 const app = express();
+app.use(express.json()); //parsing http request
+app.use(express.urlencoded({extended:true}))
+
 mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/tcart',{
 //     useNewUrlParser: true,
 //   useUnifiedTopology: true,
@@ -14,21 +18,9 @@ mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/tcart',{
 
 
 
-app.get('/api/products/:id',(req,res)=>{
-    const product=data.products.find(x=>x._id===req.params.id);
-    if(product){
-        res.send(product);
-    }
-    else{
-        res.status(404).send({message:'Product not found'});
-    }
-})
-
-app.get('/api/products', (req, res) => {
-    res.send(data.products);
-});
-
 app.use('/api/users',userRouter);
+
+app.use('/api/products',productRouter)
 
 app.get('/', (req, res) => {
     res.send('Server is ready');
